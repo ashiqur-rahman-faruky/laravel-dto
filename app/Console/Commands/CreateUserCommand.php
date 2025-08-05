4ca4f905939a7b60352aca4078a1d66a173a6926
Md Ashiqur Rahman <53116858+ashiqur-rahman-faruky@users.noreply.github.com>
2025-08-05 22:39:26 +0600

    Create CreateUserCommand.php

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Console\Commands;
+use Illuminate\Console\Command;
+use App\DTOs\CreateUserDTO;
+use App\Services\UserService;
+
+class CreateUserCommand extends Command
+{
+    protected $signature = 'user:create {name} {email} {password}';
+    protected $description = 'Create a new user from CLI';
+
+    public function handle(UserService $userService)
+    {
+        $dto = new CreateUserDTO(
+            name: $this->argument('name'),
+            email: $this->argument('email'),
+            password: $this->argument('password'),
+        );
+
+        $userService->create($dto);
+
+        $this->info('User created successfully!');
+    }
+}
